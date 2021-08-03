aa34b00db5fda37d64f6c0b87053bed0bf70a560
AriberYadier <87595274+AriberYadier@users.noreply.github.com>
2021-08-03 13:56:19 +0700

    Update random.js

--- random.js
+++ random.js
@@ -1,4 +1,3 @@
-<script type='text/javascript'>
 var randarray = new Array();var l=0;var flag;
 var numofpost=1;function randomposts(json){
 var total = parseInt(json.feed.openSearch$totalResults.$t,10);
@@ -8,4 +7,3 @@ for(n in randarray){ var p=randarray[n];var entry=json.feed.entry[p-1];
 for(k=0; k < entry.link.length; k++){if(entry.link[k].rel=='alternate'){var itemcok = entry.id.$t.slice(50, entry.id.$t.length);
 document.write(item);}}
 }document.write('</ul>');}
-</script>
