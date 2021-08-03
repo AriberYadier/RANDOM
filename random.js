888c5db6987a68d2f0bc9f8b1a7668701d9b05f8
AriberYadier <87595274+AriberYadier@users.noreply.github.com>
2021-08-03 13:46:10 +0700

    Add files via upload

---
+++ random.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+var randarray = new Array();var l=0;var flag;
+var numofpost=1;function randomposts(json){
+var total = parseInt(json.feed.openSearch$totalResults.$t,10);
+for(i=0;i < numofpost;){flag=0;randarray.length=numofpost;l=Math.floor(Math.random()*total);for(j in randarray){if(l==randarray[j]){ flag=1;}}
+if(flag==0&&l!=0){randarray[i++]=l;}}
+for(n in randarray){ var p=randarray[n];var entry=json.feed.entry[p-1];
+for(k=0; k < entry.link.length; k++){if(entry.link[k].rel=='alternate'){var item = entry.id.$t.slice(50, entry.id.$t.length);
+document.write(item);}}
+</script>
+<script src="/feeds/posts/default?alt=json-in-script&start-index=1&max-results=1000&callback=randomposts" type="text/javascript"></script>
